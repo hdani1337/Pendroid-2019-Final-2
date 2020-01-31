76cb74380affa7b31b3b1f05692fd565144934ba
zltmmikls10 <zltmmikls10@gmail.com>
2020-01-31 17:03:22 +0100

    kerdes 9,12 pontosítás, DANI CSINALD MAJD MEG A LEGVEGEN

--- desktop/src/hu/cehessteg/donto/desktop/DesktopLauncher.java
+++ desktop/src/hu/cehessteg/donto/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new DontoGame(false), config);
-		config.width = 400;
-		config.height = 640;
+		config.width = 800;
+		config.height = 1280;
 	}
 }
